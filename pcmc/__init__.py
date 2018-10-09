99f4b9b7dea069f6d7498c656a9feb76bec4b9ce
havocesp <umpierrez@pm.me>
2018-10-09 22:22:02 +0200

    Changed license to UNLICENSE and added __dependencies__ variable.

--- pcmc/__init__.py
+++ pcmc/__init__.py
@@ -8,12 +8,12 @@ from pcmc.core import CoinMarketCap
 __project__ = 'Pandas CoinMarketCap'
 __package__ = 'pcmc'
 __version__ = '0.1.3'
-__license__ = 'MIT'
+__license__ = 'UNLICENSE'
 __author__ = 'Daniel J. Umpierrez'
 __site__ = 'https://github.com/havocesp/{}'.format(__package__)
 __description__ = __doc__
 __email__ = 'umpierrez@pm.me'
 __keywords__ = 'api-wrapper coinmarketcap crytocurrencies altcoins altcoin bitcoin exchange data stock finance'
-
+__dependencies__ = ['requests', 'tabulate', 'pandas', 'ccxt', 'begins', 'git+https://github.com/havocesp/cryptocmp.git']
 __all__ = ['CoinMarketCap', '__package__', '__description__', '__version__', '__license__', '__author__', '__site__',
            '__project__', '__email__', '__keywords__']
