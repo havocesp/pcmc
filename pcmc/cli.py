92ac804c66f7e58f6e45a4c34ff840642a06f92f
havocesp <umpierrez@pm.me>
2018-10-09 16:37:10 +0200

    Added cache handler function and needed variables. Added some functions documentation with doctests.

--- pcmc/cli.py
+++ pcmc/cli.py
@@ -4,14 +4,18 @@
  - Created:     05-15-18 00:10
  - GitHub:      https://github.com/havocesp/pcmc
 """
+import json
 import time
+import typing as tp
 import warnings
+from pathlib import Path
 
 import begin
 import ccxt
 import pandas as pd
 import tabulate as tbl
 import term
+from appdirs import AppDirs
 from begin.utils import tobool
 
 from pcmc import CoinMarketCap
@@ -19,31 +23,95 @@ from pcmc import CoinMarketCap
 warnings.filterwarnings('ignore')
 
 TIMEFRAMES = ['1h', '24h', '7d']
+HOME = AppDirs('ccxt')
+HOME_CONFIG = Path(HOME.user_config_dir)
+HOME_LOCAL_SHARE = Path(HOME.user_data_dir)
+
+HOME_CONFIG.mkdir(parents=True, exist_ok=True)
+HOME_LOCAL_SHARE.mkdir(parents=True, exist_ok=True)
 
 SETTINGS = dict(timeout=30000)
 
 
+def markets_cache_handler(api):
+    """
+    Load data from cached file (data is refreshed every 24h)
+
+    :param ccxt.Exchange api:
+    :return dict: markets metadata for exchange name extracted from "api" param.
+    """
+    MARKETS_DIR = HOME_LOCAL_SHARE.joinpath('markets')
+    MARKETS_DIR.mkdir(parents=True, exist_ok=True)
+    DATA_FILE = MARKETS_DIR.joinpath('{}.json'.format(api.id))
+    DATA_FILE.touch(exist_ok=True)
+
+    file_date = DATA_FILE.stat().st_ctime
+    file_size = DATA_FILE.stat().st_size
+
+    ts_now = int(time.time())
+
+    less_than_24h = (ts_now - file_date) < (3600 * 24)
+
+    if file_size > 0 and less_than_24h:
+        raw = DATA_FILE.read_text()
+        data = json.loads(raw)
+        return data
+    else:
+        markets = api.load_markets()
+        data = json.dumps(markets, indent=4)
+        DATA_FILE.write_text(data)
+        return markets
+
+
 def get_exchange_currencies(exchange):
+    """
+    Returns all supported currencies for "exchange".
+
+    >>> get_exchange_currencies('hitbtc')[:4]
+    ['1ST', 'ABA', 'ABTC', 'ABYSS']
+
+    :param str exchange: exchange name
+    :return list: all supported currencies for "exchange"
+    """
     e = str(exchange).lower()
-    api = getattr(ccxt, e)
-    api = api(SETTINGS)
-    symbols = api.load_markets().keys()
-    currencies = sorted({s.split('/')[0] for s in symbols})
+    api = getattr(ccxt, e)(SETTINGS)  # type: ccxt.Exchange
+    api.markets = markets_cache_handler(api)
+    split = lambda s: str(s).split('/')
+    currencies = {s[0] for s in map(split, api.symbols)}
     return list(currencies)
 
 
 def _get_last_version(e):
+    """
+    Return latest exchange version reference from ccxt exchange list.
+
+    >>> _get_last_version('hitbtc')
+    'hitbtc2'
+
+    :return str: latest exchange version name available at "ccxt" lib.
+    """
     return [x for x in ccxt.exchanges if x.rstrip('123_ ') in e][-1]
 
 
 def get_uniq_currency_list(currencies):
-    currency_lists = list(currencies.values())
+    """
+    Returns a sorted and deduped currency list from "currencies" param data.
+    :param tp.Iterable currencies:
+    :return:
+    """
+    currency_lists = list(currencies.values()) if isinstance(currencies, dict) else list(currencies)
     flat_currency_lists = sum(currency_lists, [])
     all_currencies = list(set(flat_currency_lists))
-    return sorted(all_currencies)
+    return list(sorted(all_currencies))
 
 
 def filter_by_exchanges(data, exchanges):
+    """
+
+    :param pd.DataFrame data:
+    :param tp.Iterable[tp.AnyStr] exchanges:
+    :return pd.DataFrame:
+    """
     currencies = {e: get_exchange_currencies(e) for e in exchanges}
     all_currencies = get_uniq_currency_list(currencies)
     data = data[[c in all_currencies for c in data.symbol.tolist()]]
@@ -83,11 +151,12 @@ def main(timeframe='1h', filter_by='gainers', loop=False, loop_interval=60, *exc
             data['volume24h'] = data['volume24h'].apply(lambda x: str(format(float(x), ' >12,.0f') + ' $').rjust(15))
             final = filter_by_exchanges(data, exchanges)  # type: pd.DataFrame
             final = final[columns].rename(rename, axis=1)
+            term.clear(), term.pos(1, 1)
             print(tbl.tabulate(final.to_records(False), **table_settings))
-        except Exception as err:
+        except IndexError as err:
             print(type(err), ': ', str(err))
         except KeyboardInterrupt:
             user_exit = True
         finally:
-            if not user_exit:
+            if not user_exit and loop:
                 time.sleep(loop_interval)
