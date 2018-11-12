1f8d2b232abb3ae554bcb078426911715ce8fdf7
Daniel J. Umpierrez <umpierrez@pm.me>
2018-11-12 01:12:55 +0100

    Added "bs4" package for a better site scrapping. Added "pandas_settings" function for options configuration. Now, latest request will be cached for a better performance. Added "get_price", "get_exchange_symbols", "get_exchange_currencies", "get_markets_by", "get_all", "get_currency_exchanges" and "get_exchanges" methods.

--- pcmc/core.py
+++ pcmc/core.py
@@ -6,55 +6,16 @@ Core module
  - GitHub:      https://github.com/havocesp/pcmc
 """
 import re
-import sys
-import time
 import typing as tp
 
+import bs4
 import pandas as pd
-import requests
+import pcmc.static as st
+from pcmc.utils import data2num, epoch, get_url, pandas_settings
 
-from pcmc.utils import data2nums
+pandas_settings()
 
-# noinspection PyUnusedName
-pd.options.display.precision = 8
-
-_URL_BASE = 'http://coinmarketcap.com/{}'
-
-# _URL_ALL = _URL_BASE.format('all/views/all/')
-_URL_GAINERS_LOSERS = _URL_BASE.format('gainers-losers/')
-
-NEW_NAMES = {'Volume (24h)': 'volume24h',
-             'Name': 'name',
-             'Symbol': 'symbol',
-             'Price': 'price',
-             '% 1h': '1h',
-             '% 24h': '24h',
-             '% 7d': '7d',
-             'Market Cap': 'market_cap',
-             'Circulating Supply': 'circulating'}
-
-GAINERS_LOSERS_FIELDS = ['symbol', 'name', 'usd', 'btc', 'volume24h', '1h']
-
-
-def extract_rate(text, currency='BTC'):
-    """
-    Extract USD to "currency" rate from html code in "text".
-
-    :param str text: html code.
-    :param str currency: 3 chars length fiat (or "BTC") currency name.
-    :return float: usd to "currency" exchange rate as float.
-    """
-    currency = str(currency).lower()
-    pattern = 'data-{}.+"[0-9]+([\.][0-9])*["]'.format(currency)
-    pattern = re.compile(pattern)
-
-    result = pattern.search(text)
-    if result and isinstance(result, tp.Match) and len(result.span()):
-        result = str(result.group())
-        result = result.split('"')
-        result = result[1] if len(result) else result
-        return float(result.strip(' "'))
-    return result or 1.0
+_PATTERN = 'data-{}.+"[0-9]+([\.][0-9])*["]'
 
 
 # noinspection PyUnusedFunction
@@ -70,154 +31,328 @@ class CoinMarketCap:
     >>> gainers_1h['btc'].empty
     False
     """
+    _all_currencies = pd.DataFrame()
+    _cache = dict()
 
     @classmethod
-    def _get(cls, url):
+    def _fetch_url(cls, url, retries=5):
         """
-        Read url and return one DataFrame per HTML table found.
+        Fetch url then return its content (after save it on cache).
 
-        :param str url:
+        :param str url: URL to fetch.
+        :return str: raw URL content as str.
+        """
+        if url not in cls._cache or epoch() - cls._cache.get(url).get('updated', 0.0) > 3:
+            cls._cache.update({
+                url: {
+                    'data': get_url(url, retries, 10),
+                    'updated': epoch()
+                }
+            })
+        return cls._cache[url]['data']
+
+    @classmethod
+    def _scrapper(cls, url, match=None):
+        """
+        CoinMarketCap site scrapper.
+
+        Once URL fetching process is done, is fetched reading done site scrap code as str,  parse every HTML table DataFrame from HTML table data enclosed inside HTML table tags and  found in URL, a pandas DataFrame is generated  by processing enclosed data its  HTML table data for every HTML table found and finally a list instance will be returned containing one DataFrame  (html table a pandas DataFrame
+
+        :param tp.AnyStr url: CoinMarketCap URL (including endpoint)
         :return tp.List[pd.DataFrame]: list of pandas DatFrame instances (one per table tag found in "url")
         """
-        headers = {'Referer': 'http://coinmarketcap.com/gainers-losers/',
-                   'Cache-Control': 'no-cache',
-                   'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu'
-                                 ' Chromium/69.0.3497.81 Chrome/69.0.3497.81 Safari/537.36',
-                   'Pragma': 'no-cache'}
+        # cache data is considered as expired when its older than 3 secs
+        raw = cls._fetch_url(url)
+        df_list = pd.read_html(raw, match=match or r'.+')  # type: pd.DataFrame
 
-        df_list = list()
+        if len(df_list) > 1:
+            return [cls._data_handler(tbl) for idx, tbl in enumerate(df_list)]
+        elif len(df_list):
+            return cls._data_handler(df_list[0])
+        else:
+            return list()
 
-        response_text = str()
-        while not len(df_list):
+    @classmethod
+    def _data_handler(cls, data):
+        """
+        Do some data processing with columns (formatting, currency conversion, remove unnecessary data, ...)
 
-            response = None
-            try:
-                response = requests.get(url, headers=headers)
-                if response.ok:
-                    response_text = response.text
-                    df_list = pd.read_json(response_text or str())
-                else:
-                    print(' - [ERROR] Request error, retrying in 10 seconds ...')
-                    time.sleep(10)
-                    print(' - Retrying ...')
-            except requests.exceptions.HTTPError as err:
-                print('[74] HTTPError: ', str(err))
-                if response:
-                    df_list = pd.read_html(response_text or str())
-            except ValueError as err:
-                if response:
-                    df_list = pd.read_html(response_text or str())
-            except Exception as err:
-                print('[81] Unknow: ', type(err), ': ', str(err))
-                if response:
-                    df_list = pd.read_html(response_text or str())
-            except KeyboardInterrupt:
-                return sys.exit(1)
-        rate = extract_rate(response_text) or 1.0
-        return dict(data=df_list, rate=rate)
+        :param pd.DataFrame data: DataFrame to be processed.
+        :return pd.DataFrame: resulting data.
+        """
+
+        # infer 1h, 24h or 7d timeframe from column name ending in "h" or "d" and then remove "%" char
+        timeframe = [c[2:] for c in data.columns if c[-1] in ['h', 'd']]
+
+        data = data.drop('#', axis=1)
+        data = data.rename(index=str, columns=st.NEW_NAMES)
+        data = data.applymap(data2num)
+        # USD price to BTC conversion
+        data['btc'] = data['usd'] / cls.get_price('BTC')
+
+        return data[st.GAINERS_LOSERS_FIELDS + timeframe]
 
     @property
     def gainers_and_losers(self):
+        """Filter response for getting gainers and losers data and return it as dict with "gainers" and "losers" keys.
+
+        :return tp.Dict: dict with gainers and losers keys containing its respective data.
         """
-        Filter response for getting gainers and losers data and return it as dict with "gainers" and "losers" keys.
-        :return dict:
-        """
-        result = dict(rate=0.0)
-        raw = self._get(_URL_GAINERS_LOSERS)
-        tables = raw.get('data', list())
-        rate = raw.get('rate', 1.0)
-        if len(tables):
-
-            tables = [t.drop('#', axis=1).rename(index=str, columns=NEW_NAMES).applymap(data2nums) for t in tables]
-
-            for idx in range(len(tables)):
-                tables[idx]['btc'] = tables[idx]['price'] / rate
-
-            gainers = {'1h': tables[0], '24h': tables[1], '7d': tables[2]}
-            losers = {'1h': tables[3], '24h': tables[4], '7d': tables[5]}
-            result.update(gainers=gainers, losers=losers, rate=rate)
-        return result
+        data = self._scrapper(st.URL_GAINERS_LOSERS)
+        gainers = {'1h': data[0], '24h': data[1], '7d': data[2]}
+        losers = {'1h': data[3], '24h': data[4], '7d': data[5]}
+        return dict(gainers=gainers, losers=losers)
 
     @property
     def gainers(self):
         """
-        Return all gainers data as dict as pandas dataframe.
+        Return gainers data as dict with 1h, 24h and 7d keys with respective data as DataFrames.
 
-        :return dict: all gainers data as dict as pandas dataframe.
+        :return dict: gainers data as dict with 1h, 24h and 7d keys with respective data as DataFrames.
         """
         return self.gainers_and_losers['gainers']
 
     @property
     def losers(self):
         """
-        Return all losers data as dict as pandas dataframe..
+        Return dict with losers "1h", "24h" and "7d" DataFrames.
 
-        :return dict: all losers data as dict as pandas dataframe.
+        :return dict: losers data as dict with 1h, 24h and 7d keys with respective data as DataFrames.
         """
         return self.gainers_and_losers['losers']
 
     @property
     def gainers_1h(self):
         """
-        Return last hour gainers data.
+        Returns a DataFrame instance with last hour gainers data.
 
-        :return pd.DataFrame: last hour gainers data
+        >>> CoinMarketCap().gainers_1h
+
+        :return pd.DataFrame: a DataFrame instance with last hour (1h) gainers data.
         """
-        hour = self.gainers['1h']  # type: pd.DataFrame
-        hour['usd'] = hour.pop('price')
-        return hour[GAINERS_LOSERS_FIELDS]
+        data = self._scrapper(st.URL_GAINERS_LOSERS, match=r'% 1h')
+        return data[0] if data and len(data) else pd.DataFrame()
 
     @property
     def gainers_24h(self):
         """
-        Return last day gainers data.
+        Returns a DataFrame instance with last day gainers data.
 
-        :return pd.DataFrame: last day gainers data as pandas dataframe.
+        :return pd.DataFrame: a DataFrame instance with last day (24h) gainers data.
         """
-        day = self.gainers['24h']  # type: pd.DataFrame
-        day['usd'] = day.pop('price')
-        return day[GAINERS_LOSERS_FIELDS]
+        data = self._scrapper(st.URL_GAINERS_LOSERS, match=r'% 24h')
+        return data[0] if data and len(data) else pd.DataFrame()
 
     @property
     def gainers_7d(self):
         """
-        Return last week gainers data.
+        Returns a DataFrame instance with last week gainers data.
 
-        :return pd.DataFrame: last week gainers data as pandas dataframe.
+        :return pd.DataFrame: a DataFrame instance with last week (7d) gainers data.
         """
-        week = self.gainers['7d']  # type: pd.DataFrame
-        week['usd'] = week.pop('price')
-        return week[GAINERS_LOSERS_FIELDS]
+        data = self._scrapper(st.URL_GAINERS_LOSERS, match=r'% 7d')
+        return data[0] if data and len(data) else pd.DataFrame()
 
     @property
     def losers_1h(self):
         """
-        Return last hour losers data.
+        Returns a DataFrame instance with last hour losers data.
 
-        :return pd.DataFrame: last hour losers data as pandas dataframe.
+        :return pd.DataFrame: a DataFrame instance with last hour (1h) losers data.
         """
-        hour = self.losers['1h']  # type: pd.DataFrame
-        hour['usd'] = hour.pop('price')
-        return hour[GAINERS_LOSERS_FIELDS]
+        data = self._scrapper(st.URL_GAINERS_LOSERS, match=r'% 1h')
+        return data[-1] if data and len(data) else pd.DataFrame()
 
+    #
     @property
     def losers_24h(self):
         """
-        Return last day losers data as pandas dataframe..
+        Returns a DataFrame instance with last day losers data.
 
-        :return pd.DataFrame: last day losers data as pandas dataframe.
+        :return pd.DataFrame: a DataFrame instance with last day (24h) losers data.
         """
-        day = self.gainers['24h']  # type: pd.DataFrame
-        day['usd'] = day.pop('price')
-        return day[GAINERS_LOSERS_FIELDS]
+        data = self._scrapper(st.URL_GAINERS_LOSERS, match=r'% 24h')
+        return data[-1] if data and len(data) else pd.DataFrame()
 
     @property
     def losers_7d(self):
         """
-        Return last week losers data.
+        Returns a DataFrame instance with last week losers data.
 
-        :return pd.DataFrame: last week losers data as pandas dataframe.
+        :return pd.DataFrame: a DataFrame instance with last week (7d) losers data.
         """
-        week = self.gainers['7d']  # type: pd.DataFrame
-        week['7d'] = week.pop('7d')
-        return week[GAINERS_LOSERS_FIELDS]
+        data = self._scrapper(st.URL_GAINERS_LOSERS, match=r'% 7d')
+        return data[-1] if data and len(data) else pd.DataFrame()
+
+    @classmethod
+    def get_price(self, currency):
+        """
+        Extract USD to "currency" rate from html code in "text".
+
+        >>> rate = CoinMarketCap().get_price()
+        >>> isinstance(rate, float)
+        True
+
+        :param str currency: 3 chars length fiat (or "BTC") currency name.
+        :return float: usd to "currency" exchange rate as float.
+        """
+        currency = str(currency).lower()
+        data = self._fetch_url(st.URL_GAINERS_LOSERS)
+        result = re.search(_PATTERN.format(currency), data)
+        if result and hasattr(result, 'span'):
+            result = str(result.group())
+            result = result.split('"')
+            result = result[1] if len(result) else result
+            return float(result.strip(' "'))
+        return result or 1.0
+
+    @classmethod
+    def get_exchange_symbols(cls, exchange, quote_currency=None):
+        """
+        Get symbol supported by a given exchange (optionally filtered by a base market)
+
+        :param str exchange: exchange name used on request.
+        :param str quote_currency: only symbols matching "quote_currency" value will be returned.
+        :return list: exchange supported symbols as list
+        """
+        exchange = str(exchange).lower()
+        data = cls._fetch_url(st.URL_EXCHANGES.format(exchange))
+        if data and isinstance(data, str) and len(data):
+            data = pd.read_html(data)
+            data = data.pop(0)
+            symbols = data['Pair']  # type: pd.Series
+
+            if quote_currency:
+                quote_currency = str(quote_currency).upper()
+                markets = {s.split('/')[1] for s in cls.get_exchange_symbols(exchange)}
+                if quote_currency in markets:
+                    symbols = symbols.select(lambda s: symbols[s].split('/')[1] in quote_currency)
+            return symbols.sort_values().tolist()
+
+    @classmethod
+    def get_exchange_currencies(cls, exchange):
+        """
+        Get supported currencies by exchange.
+
+        :param str exchange: exchange name used on request.
+        :return list: exchange supported currencies as list.
+        """
+        currencies = {s.split('/')[0] for s in cls.get_exchange_symbols(exchange)}
+        return sorted(currencies)
+
+    @classmethod
+    def get_markets_by(cls, exchange):
+        """
+        Get exchange supported markets as list.
+
+        :param str exchange: exchange name used on request.
+        :return list: exchange supported markets as list.
+        """
+        markets = {s.split('/')[1] for s in cls.get_exchange_symbols(exchange)}
+        return sorted(markets)
+
+    @classmethod
+    def get_all(cls):
+        if not len(cls._all_currencies):
+            data = cls._fetch_url(st.URL_ALL)
+            scrapper = bs4.BeautifulSoup(data, features='lxml')
+            table = scrapper.find('table', attrs={'id': 'currencies-all'})
+            names_tags = scrapper.find_all('a', attrs={'class': 'link-secondary'})
+            names = dict()
+            num = len(names_tags) // 2
+
+            for i in range(0, num, 2):
+                long_name = names_tags[i + 1]['href'].split('/')[2]
+                short_name = names_tags[i].text
+                names.update(**{short_name: long_name})
+
+            data = [tr.text.replace('\n\n', '@').replace('\n', '').replace(',', '').replace('$', '').replace('*',
+                                                                                                             '').lstrip(
+                '@12345677890').split('@')[1:8] for tr in table.find_all('tr')]
+
+            final = list()
+            for row in list(data):
+                if len(row) > 5:
+                    row = [data2num(r) for r in row]
+
+                    if isinstance(row[-1] or 0, str) and '%' in row[-1] and '?' not in row[-1]:
+                        tmp = row[-1].split('%')[:3]
+                        row[-1] = float(tmp.pop(0))
+                        row.extend(list(map(float, tmp)))
+                        if row[0] == row[1]:
+                            row[0] = names.get(row[1])
+                        else:
+                            row[0] = row[0].upper()
+                        final.append(row)
+
+            df = pd.DataFrame(final, columns=st.ALL_FIELDS)
+
+            df = df[[True if isinstance(data2num(v), float) else False for v in df['volume24h']]]
+            df['usd'] = df['usd'].apply(data2num)
+            df['btc'] = df['usd'] / cls.get_price('BTC')
+            df['usd'] = df['usd'].round(2)
+            df['btc'] = df['btc'].round(8)
+            btc = df.pop('btc')
+            df.insert(2, column='btc', value=btc)
+            df['market_cap'] = df['market_cap'].apply(data2num).apply(int)
+            df['1h'] = df['1h'].apply(data2num).round(2)
+            df['24h'] = df['24h'].apply(data2num).round(2)
+            df['7d'] = df['7d'].apply(data2num).round(2)
+            df['volume24h'] = df['volume24h'].apply(data2num).apply(int)
+
+            df = df.set_index('symbol')
+            cls._all_currencies = df.copy(True)
+        return cls._all_currencies
+
+    @classmethod
+    def get_currency_exchanges(cls, currency):
+        """
+        Get exchanges where the supplied currency is currently supported.
+
+        >>> exchanges = cls.get_currency_exchanges('CHAT')
+        >>> isinstance(exchanges, list) and len(exchanges)
+        True
+
+        :param str currency: desired currency used for data request.
+        :return list: exchange list where currency is supported as list.
+        """
+        ac = cls.get_all()
+        long_name = ac.T[str(currency).upper()]['name']
+        data = cls._fetch_url(st.URL_CURRENCIES.format(long_name.lower()))
+        data = pd.read_html(data)
+        df = data.pop(0)  # type: pd.DataFrame
+        symbols = df['Source'].sort_values()
+        return symbols.tolist()
+
+    @classmethod
+    def get_exchanges(cls, lower_case=False):
+        """
+        Get all exchanges listed on CoinMarketCap.
+
+        :param bool lower_case: if True, exchange names will be lower cased before return.
+        :return list: exchanges listed on CoinMarketCap as list.
+        """
+        data = cls._fetch_url(st.URL_EXCHANGES.format(''))
+        data = pd.read_html(data)
+        df = data.pop(0)  # type: pd.DataFrame
+
+        exchanges = df['Name']
+
+        if lower_case:
+            exchanges = exchanges.apply(str.lower)
+
+        return exchanges.tolist()
+
+#
+# if __name__ == '__main__':
+#     CMC = CoinMarketCap
+#     exchanges = CMC.get_currency_exchanges('XRP')
+#     data = CMC.get_all()
+#     binance_currencies = CMC.get_exchange_currencies('binance')
+#     hitbtc_currencies = CMC.get_exchange_currencies('hitbtc')
+#     cryptopia_currencies = CMC.get_exchange_currencies('cryptopia')
+#     all_currencies = sorted(set(hitbtc_currencies + binance_currencies + cryptopia_currencies))
+#
+#     print(data.sort_values(['1h'], ascending=False))
+#     # print(data.sort_values(['24h'], ascending=False))
+#     # print(CMC.get_currency_exchanges('CHAT'))
