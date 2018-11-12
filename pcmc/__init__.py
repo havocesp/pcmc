c68d89f589c433fb57f6ba63686f6a39891579f1
Daniel J. Umpierrez <umpierrez@pm.me>
2018-11-12 01:06:55 +0100

    Update requirements, app version and mistake corected related with README.md file read.

--- pcmc/__init__.py
+++ pcmc/__init__.py
@@ -6,7 +6,7 @@ import pathlib
 
 from pcmc.core import CoinMarketCap
 
-_CWD = pathlib.Path.cwd()
+_CWD = pathlib.Path(__file__).parent  # type: pathlib.Path
 
 PATH_ROOT = _CWD.parent  # type: pathlib.Path
 
@@ -16,13 +16,13 @@ __long_description__ = __long_description__.read_text(encoding='utf-8')
 
 __project__ = 'Pandas CoinMarketCap'
 __package__ = 'pcmc'
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 __license__ = 'UNLICENSE'
 __author__ = 'Daniel J. Umpierrez'
 __site__ = 'https://github.com/havocesp/{}'.format(__package__)
 __description__ = __doc__
 __email__ = 'umpierrez@pm.me'
 __keywords__ = 'api-wrapper coinmarketcap crytocurrencies altcoins altcoin bitcoin exchange data stock finance'
-__dependencies__ = ['requests', 'tabulate', 'pandas', 'ccxt', 'begins']
+__dependencies__ = ['tabulate', 'pandas', 'bs4', 'py-term']
 __all__ = ['CoinMarketCap', '__package__', '__description__', '__version__', '__license__', '__author__', '__site__',
            '__project__', '__email__', '__keywords__']
