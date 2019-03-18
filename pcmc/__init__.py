69345fd36ac353d268ac8d664258f7bbca9366f5
Daniel J. Umpierrez <umpierrez@pm.me>
2019-03-18 04:22:27 +0100

    __dependencies__ moved to setup.py and __version__ updated

--- pcmc/__init__.py
+++ pcmc/__init__.py
@@ -14,13 +14,13 @@ __long_description__ = __long_description__.read_text(encoding='utf-8')
 
 __project__ = 'pcmc'
 __package__ = 'pcmc'
-__version__ = '0.1.6'
+__version__ = '0.1.8'
 __license__ = 'UNLICENSE'
 __author__ = 'Daniel J. Umpierrez'
 __site__ = 'https://github.com/havocesp/{}'.format(__package__)
 __description__ = __doc__
 __email__ = 'umpierrez@pm.me'
 __keywords__ = 'api-wrapper coinmarketcap crypto-currencies altcoins altcoin bitcoin exchange data stock finance'
-__dependencies__ = ['tabulate', 'pandas', 'bs4', 'py-term']
+
 __all__ = ['CoinMarketCap', '__package__', '__description__', '__version__', '__license__', '__author__', '__site__',
            '__project__', '__email__', '__keywords__']
